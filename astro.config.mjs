d98ba2638f8ab2f7804cc433b0d6b4a5c65e9a40
v-i-s-h-a-l <5095154+v-i-s-h-a-l@users.noreply.github.com>
2026-06-17 04:52:54 +0530

    feat: scaffold knowledge garden site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://v-i-s-h-a-l.github.io",
+  base: "/knowledge",
+  output: "static",
+  trailingSlash: "always"
+});
